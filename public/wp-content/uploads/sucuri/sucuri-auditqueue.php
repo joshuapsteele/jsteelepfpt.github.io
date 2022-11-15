55881c80280680e4ce7d64508addacbd38608460
Joshua Steele <jsteele@proofpoint.com>
2022-11-15 15:59:14 -0500

    Build side and edit title

---
+++ public/wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1668426469;
+// updated_on=1668426469;
+exit(0);
+?>
+1668426469_1479:"Debug: joshuapsteele, 71.112.188.168; Sucuri plugin has been uninstalled"
---
+++ public/wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1668426469;
+// updated_on=1668426469;
+exit(0);
+?>
